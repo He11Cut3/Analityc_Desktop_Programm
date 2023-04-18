161ac5939a86a741a8429026ab58f0780584829e
He11Cut3 <ruslan.off89@mail.ru>
2023-04-18 22:46:13 +0300

    Version with Excel Report

--- Analytic/Main.xaml.cs
+++ Analytic/Main.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Analytic.Edit;
 using Analytic.User_Control;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
@@ -198,6 +201,124 @@ namespace Analytic
 
             UC_Recipe uC_Recipe = new UC_Recipe();
             LV_Recipe.Children.Add(uC_Recipe);
+        }
+
+        private void Report_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((MessageBox.Show("Вы уверены, что хотите создать отчёт?", "Изменение", MessageBoxButton.YesNo, MessageBoxImage.Warning)) == MessageBoxResult.Yes)
+            {
+                Day_Ex();
+                Week_Ex();
+                Month_Ex();
+            }
+        }
+        public void Day_Ex()
+        {
+
+            List<Analityc_Plan_Day> day = _context.Analityc_Plan_Day.ToList();
+
+
+            // Определяем наименования столбцов
+            string[] columnNames = new string[] { "Номенклатура (День)", "Выполнить до(День)", "Статус выполнения(День)", "Обьем(День)", "Примечание(День)" };
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // Создаем новый файл Excel
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                // Добавляем лист
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("День");
+
+                // Записываем наименования столбцов
+                for (int i = 0; i < columnNames.Length; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = columnNames[i];
+                }
+
+                // Записываем данные
+                for (int i = 0; i < day.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = day[i].Analityc_Plan_Day_Nomenclature;
+                    worksheet.Cells[i + 2, 2].Value = day[i].Analityc_Plan_Day_Volume;
+                    worksheet.Cells[i + 2, 3].Value = day[i].Analityc_Plan_Day_Note;
+                    worksheet.Cells[i + 2, 4].Value = day[i].Analityc_Plan_Day_Date;
+                    worksheet.Cells[i + 2, 5].Value = day[i].Analityc_Plan_Day_Status;
+                }
+
+                // Сохраняем файл
+                File.WriteAllBytes("День.xlsx", package.GetAsByteArray());
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string filePath = System.IO.Path.Combine(desktopPath, "День.xlsx");
+                File.WriteAllBytes(filePath, package.GetAsByteArray());
+            }
+        }
+        public void Week_Ex()
+        {
+            List<Analityc_Plan_Week> week = _context.Analityc_Plan_Week.ToList();
+            // Определяем наименования столбцов
+            string[] columnNames = new string[] { "Номенклатура(Неделя)", "Выполнить до(Неделя)", "Статус выполнения(Неделя)", "Обьем(Неделя)", "Примечание(Неделя)" };
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // Создаем новый файл Excel
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                // Добавляем лист
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Неделя");
+
+                // Записываем наименования столбцов
+                for (int i = 0; i < columnNames.Length; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = columnNames[i];
+                }
+
+                // Записываем данные
+                for (int i = 0; i < week.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = week[i].Analityc_Plan_Week_Nomenclature;
+                    worksheet.Cells[i + 2, 2].Value = week[i].Analityc_Plan_Week_Volume;
+                    worksheet.Cells[i + 2, 3].Value = week[i].Analityc_Plan_Week_Note;
+                    worksheet.Cells[i + 2, 4].Value = week[i].Analityc_Plan_Week_Date;
+                    worksheet.Cells[i + 2, 5].Value = week[i].Analityc_Plan_Week_Status;
+                }
+
+                // Сохраняем файл
+                File.WriteAllBytes("Неделя.xlsx", package.GetAsByteArray());
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string filePath = System.IO.Path.Combine(desktopPath, "Неделя.xlsx");
+                File.WriteAllBytes(filePath, package.GetAsByteArray());
+            }
+        }
+        public void Month_Ex()
+        {
+            List<Analityc_Plan_Month> month = _context.Analityc_Plan_Month.ToList();
+            // Определяем наименования столбцов
+            string[] columnNames = new string[] { "Номенклатура(Месяц)", "Выполнить до(Месяц)", "Статус выполнения(Месяц)", "Обьем(Месяц)", "Примечание(Месяц)" };
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // Создаем новый файл Excel
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                // Добавляем лист
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Месяц");
+
+                // Записываем наименования столбцов
+                for (int i = 0; i < columnNames.Length; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = columnNames[i];
+                }
+
+                // Записываем данные
+                for (int i = 0; i < month.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = month[i].Analityc_Plan_Month_Nomenclature;
+                    worksheet.Cells[i + 2, 2].Value = month[i].Analityc_Plan_Month_Volume;
+                    worksheet.Cells[i + 2, 3].Value = month[i].Analityc_Plan_Month_Note;
+                    worksheet.Cells[i + 2, 4].Value = month[i].Analityc_Plan_Month_Date;
+                    worksheet.Cells[i + 2, 5].Value = month[i].Analityc_Plan_Month_Status;
+                }
+
+                // Сохраняем файл
+                File.WriteAllBytes("Месяц.xlsx", package.GetAsByteArray());
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string filePath = System.IO.Path.Combine(desktopPath, "Месяц.xlsx");
+                File.WriteAllBytes(filePath, package.GetAsByteArray());
+            }
         }
     }
 }
